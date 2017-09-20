ad779a35dac51676df1a67d5089a8829c0f0192e
Mariano Botta <marianoabotta@gmail.com>
2017-09-20 08:08:07 -0300

    Change to config private example

--- config.private.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// export const mongoHost = 'mongodb://10.1.62.17:27017/andes';
-exports.map_file = './der2_Refset_ICD10CM_US_20170301/der2_iisssccRefset_ExtendedMapSnapshot_US1000124_20170301.txt';
-exports.collection_name = 'snomed-cie10';
-exports.mongoHost = 'mongodb://localhost:27017/andes';
-//# sourceMappingURL=config.private.js.map
